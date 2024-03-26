355117b1b49ccc7e0567d9f64f418c69663bb07e
Indexhibit <vaska@indexhibit.org>
2024-03-26 15:58:13 +0800

    Update v2.1.4.php
    Upgrade bug

--- ndxzstudio/upgrade/v2.1.4.php
+++ ndxzstudio/upgrade/v2.1.4.php
@@ -13,7 +13,7 @@ class upgrade_214
 		$OBJ =& get_instance();
 		
 		// forcing a change to the users table for user ID = 1 - just in case
-		$OBJ->db->updateArray(PX.'users', array('ID' => '1'), "user_admin = '1'");
+		$OBJ->db->updateArray(PX.'users', array('user_admin' => '1'), "ID = '1'");
 		$OBJ->db->updateArray(PX.'settings', array('version' => '2.1.4'), "adm_id = '1'");
 	}
 }
